7c2fbc963fd94eed8863238beee355dff5b89da3
Akshat Gupta <84914000+akshat122402@users.noreply.github.com>
2024-06-19 12:30:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,8 +10,16 @@ python3 model.py
 
 docker-compose down
 
+CONTAINER_ID=$(docker ps -q --filter "publish=8501")
+if [ ! -z "$CONTAINER_ID" ]; then
+  docker stop $CONTAINER_ID
+  docker rm $CONTAINER_ID
+fi
+
 docker images --filter "dangling=true" -q | xargs -r docker rmi
 
 docker system prune -f
 
-docker-compose up --build -d
+docker build -t lstm_reliance_stock .
+
+docker run -p 8501:8501 lstm_reliance_stock
